c316664559c0475cd087170201a64fbe146ca503
Ricomami <don.abraham01@gmail.com>
2025-10-20 09:45:55 -0600

    Adicion de una tabla en 'auditorio-listar' para mostrar el arreglo con datos completamente locales

--- src/app/auditorio/auditorio-listar/auditorio-listar.ts
+++ src/app/auditorio/auditorio-listar/auditorio-listar.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router'
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-auditorio-listar',
-  imports: [ RouterLink ],
+  imports: [ RouterLink, CommonModule ],
   templateUrl: './auditorio-listar.html',
-  styleUrl: './auditorio-listar.css'
+  styleUrl: './auditorio-listar.css',
 })
 export class AuditorioListar {
-
+  auditorios = [
+    {id: 1, nombre: "Auditorio Benito Juarez", direccion: "Av Micasa #63232", estado: "Activo"},
+    {id: 2, nombre: "Auditorio Morelos", direccion: "Av Micasa #15166", estado: "Inactivo"},
+    {id: 3, nombre: "Auditorio Parques", direccion: "Av Micasa #98971", estado: "Inactivo"},
+    {id: 4, nombre: "Auditorio Nomadas", direccion: "Av Sucasa #91623", estado: "Activo"},
+  ];
 }
