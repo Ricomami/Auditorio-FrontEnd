67b682583a727adf8af0b26c8408ef51aa3e6ea8
Ricomami <don.abraham01@gmail.com>
2025-10-20 10:36:55 -0600

    Se agregó la carpeta Usuario con 'usuario-crear' y 'usuario-listar'

---
+++ src/app/usuario/usuario-listar/usuario-listar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-usuario-listar',
+  imports: [],
+  templateUrl: './usuario-listar.html',
+  styleUrl: './usuario-listar.css'
+})
+export class UsuarioListar {
+
+}
