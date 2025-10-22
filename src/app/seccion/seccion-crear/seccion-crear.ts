ec7f8efef0a9b1dc28c28ebc55b443ea8dd48bfb
Ricomami <don.abraham01@gmail.com>
2025-10-21 23:24:40 -0600

    Se agrego el modulo de 'src/app/seccion' con listar y crear estatico ademas del 'ngfor' para listar registros locales

--- src/app/seccion/seccion-crear/seccion-crear.ts
+++ src/app/seccion/seccion-crear/seccion-crear.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-seccion-crear',
-  imports: [],
+  imports: [ RouterLink, CommonModule],
   templateUrl: './seccion-crear.html',
   styleUrl: './seccion-crear.css'
 })
