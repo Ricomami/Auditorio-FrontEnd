44bc053f5ecd6b35f1c67a3a4433371fd19e18d2
Ricomami <don.abraham01@gmail.com>
2025-10-21 23:54:40 -0600

    Se agrego el modulo 'src/app/eventos_artistas' con 'listar' y 'crear' sin registros, y con la version con 'ngfor' para listar registros locales

--- src/app/eventos_artista/eventos-artista-listar/eventos-artista-listar.ts
+++ src/app/eventos_artista/eventos-artista-listar/eventos-artista-listar.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-eventos-artista-listar',
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './eventos-artista-listar.html',
   styleUrl: './eventos-artista-listar.css'
 })
 export class EventosArtistaListar {
+  eventos_artistas = [
+    { id_evento: "500", id_artista: "300", estado: "activo", creado: "2025-09-20T10:30:00Z", actualizado: "2025-10-15T12:45:00Z" },
+    { id_evento: "500", id_artista: "301", estado: "activo", creado: "2025-09-22T14:20:00Z", actualizado: "2025-10-10T09:35:00Z" },
+    { id_evento: "501", id_artista: "302", estado: "inactivo", creado: "2025-09-25T08:15:00Z", actualizado: "2025-10-05T11:00:00Z" },
+    { id_evento: "502", id_artista: "303", estado: "activo", creado: "2025-09-28T12:00:00Z", actualizado: "2025-10-14T18:25:00Z" },
+    { id_evento: "503", id_artista: "304", estado: "activo", creado: "2025-10-01T09:10:00Z", actualizado: "2025-10-18T20:40:00Z" },
 
+  ];
 }
