75b10373d4496076aaa8d048a2b6864bc4ae7e9d
Ricomami <don.abraham01@gmail.com>
2025-10-21 23:26:30 -0600

    Se agrego el modulo 'src/app/pago' con 'listar' y 'crear' sin registros, y con la version con 'ngfor' para listar registros locales

--- src/app/pago/pago-crear/pago-crear.ts
+++ src/app/pago/pago-crear/pago-crear.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import  {CommonModule} from '@angular/common';
+import  {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-pago-crear',
-  imports: [],
+  imports: [ RouterLink, CommonModule],
   templateUrl: './pago-crear.html',
   styleUrl: './pago-crear.css'
 })
