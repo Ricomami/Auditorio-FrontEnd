320701a80bb89c7613632e709cc1c69f3a9d80d7
Ricomami <don.abraham01@gmail.com>
2025-10-21 18:59:09 -0600

    Componentes listar y crear Usuarios integrados, con tabla y formulario con y sin columnas de la BDD

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import {AuditorioCrear} from  './auditorio/auditorio-crear/auditorio-crear';
 import {AuditorioListar} from  './auditorio/auditorio-listar/auditorio-listar';
+import {UsuarioListar} from './usuario/usuario-listar/usuario-listar'
+import {UsuarioCrear} from './usuario/usuario-crear/usuario-crear'
 
 export const routes: Routes = [
     {path: 'auditorio', component: AuditorioListar },
-    {path: 'auditorio/crear', component: AuditorioCrear}
+    {path: 'auditorio/crear', component: AuditorioCrear},
+    {path: 'usuario', component: UsuarioListar },
+    {path: 'usuario/crear', component: UsuarioCrear}
 ];
--- src/app/usuario/usuario-listar/usuario-listar.ts
+++ src/app/usuario/usuario-listar/usuario-listar.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import  {CommonModule} from '@angular/common';
+import  {RouterLink} from '@angular/router';
 
+// COMPONENTE CON LISTA FOREACH
 @Component({
   selector: 'app-usuario-listar',
-  imports: [],
+  imports: [ RouterLink, CommonModule],
   templateUrl: './usuario-listar.html',
   styleUrl: './usuario-listar.css'
 })
 export class UsuarioListar {
-
+  usuarios = [
+    //Sin tablas
+    {id: "", nombre:"", direccion:"", estado:"" },
+    //Con tablas
+    {id: "", nombre:"", contrasena:"", rol:"", imagen:"", estado:"", creado:"", actualizado:""}
+    // {id: 1, nombre: "Auditorio Benito Juarez", direccion: "Av Micasa #63232", estado: "Activo"},
+  ];
 }
