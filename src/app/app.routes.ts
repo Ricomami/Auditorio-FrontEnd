9aa56bca59e13c230c7ba58a9e1676b7a62ab4f6
Ricomami <don.abraham01@gmail.com>
2025-10-24 10:14:29 -0600

    Comienzo de proceso de planchado de vistas .html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 
+import { ArtistaListar } from './artista/artista-listar/artista-listar';
+import { ArtistaCrear } from './artista/artista-crear/artista-crear';
+import { AsientosListar } from './asientos/asientos-listar/asientos-listar';
+import { AsientosCrear } from './asientos/asientos-crear/asientos-crear';
 import {AuditorioCrear} from  './auditorio/auditorio-crear/auditorio-crear';
 import {AuditorioListar} from  './auditorio/auditorio-listar/auditorio-listar';
+import { ClientesListar } from './clientes/clientes-listar/clientes-listar';
+import { ClientesCrear } from './clientes/clientes-crear/clientes-crear';
+import { EntradasListar } from './entradas/entradas-listar/entradas-listar';
+import { EntradasCrear } from './entradas/entradas-crear/entradas-crear';
 import {EventosArtistaListar} from './eventos_artista/eventos-artista-listar/eventos-artista-listar'
 import {EventosArtistaCrear} from './eventos_artista/eventos-artista-crear/eventos-artista-crear'
+import { EventoListar } from './eventos/evento-listar/evento-listar';
+import { EventoCrear } from './eventos/evento-crear/evento-crear';
 import {FuncionListar} from './funcion/funcion-listar/funcion-listar'
 import {FuncionCrear} from './funcion/funcion-crear/funcion-crear'
 import {PagoListar} from './pago/pago-listar/pago-listar'
@@ -12,16 +22,6 @@ import {SeccionListar} from './seccion/seccion-listar/seccion-listar'
 import {SeccionCrear} from './seccion/seccion-crear/seccion-crear'
 import {UsuarioListar} from './usuario/usuario-listar/usuario-listar'
 import {UsuarioCrear} from './usuario/usuario-crear/usuario-crear'
-import { ArtistaListar } from './artistas/artista-listar/artista-listar';
-import { ArtistaCrear } from './artistas/artista-crear/artista-crear';
-import { AsientosListar } from './asientos/asientos-listar/asientos-listar';
-import { AsientosCrear } from './asientos/asientos-crear/asientos-crear';
-import { ClientesListar } from './clientes/clientes-listar/clientes-listar';
-import { ClientesCrear } from './clientes/clientes-crear/clientes-crear';
-import { EntradasListar } from './entradas/entradas-listar/entradas-listar';
-import { EntradasCrear } from './entradas/entradas-crear/entradas-crear';
-import { EventoListar } from './eventos/evento-listar/evento-listar';
-import { EventoCrear } from './eventos/evento-crear/evento-crear';
 
 
 export const routes: Routes = [
