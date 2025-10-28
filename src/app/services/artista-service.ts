487180e61f27d4191b0076b15f3e7ca4a5945333
Ricomami <don.abraham01@gmail.com>
2025-10-27 22:17:51 -0600

    Integracion del modulo artista-listar desde la BDD

--- src/app/services/artista-service.ts
+++ src/app/services/artista-service.ts
@@ -1,8 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Artista } from '../interfaces/artista'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArtistaService {
-  
+  private apiURL = "http://localhost:3000/artistas/";
+
+  constructor(private http: HttpClient) { }
+
+  obtenerArtistas(): Observable<Artista[]> {
+    return this.http.get<Artista[]>(this.apiURL);
+  }
+
+  obtenerArtistaPorId(id: number): Observable<Artista> {
+    return this.http.get<Artista>(`${this.apiURL}/${id}`);
+  }
+
+  crearArtista(artista: Artista): Observable<Artista> {
+    return this.http.post<Artista>(this.apiURL, artista);
+  }
+
+  actualizarArtista(id: number, artista: Artista): Observable<Artista> {
+    return this.http.put<Artista>(`${this.apiURL}/${id}`, artista);
+  }
+
+  eliminarArtista(id: number): Observable<any> {
+    return this.http.delete(`${this.apiURL}/${id}`);
+  }
 }
