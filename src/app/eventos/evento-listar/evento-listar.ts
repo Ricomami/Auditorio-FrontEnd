44704f3626a548a8953992b1f46b44db4a740f49
Ricomami <don.abraham01@gmail.com>
2025-10-29 07:34:15 -0600

    Integracion de 'evento/evento-listar'  con conexion a BDD

--- src/app/eventos/evento-listar/evento-listar.ts
+++ src/app/eventos/evento-listar/evento-listar.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { Evento } from '../../interfaces/evento';
+import { EventoService } from '../../services/evento-service';
 
 @Component({
   selector: 'app-evento-listar',
@@ -10,7 +12,28 @@ import { RouterLink } from '@angular/router';
   styleUrl: './evento-listar.css',
 })
 export class EventoListar {
-  eventos = [
+
+  eventos: Evento[] = [];
+  
+    constructor(private eventoServicio: EventoService) { }
+  
+    ngOnInit(): void {
+      this.cargarEntradas();
+    }
+  
+    cargarEntradas() {
+      this.eventoServicio.obtenerEventos().subscribe({
+        next: (registros: any) => {
+          console.log("Registros devueltos desde API: ", registros);
+          this.eventos = registros.datos;
+        },
+        error: (err) => {
+          console.error("Error al obtener los eventos: ", err);
+        }
+      });
+    }
+
+  eventoss = [
     {
       id:1,
       nombre_evento: 'Concierto Sinfónico',
@@ -19,7 +42,7 @@ export class EventoListar {
       fecha: new Date('2025-11-20T19:00:00'),
       hora_fin: new Date('2025-11-20T21:30:00'),
       aforo: 500,
-      auditorio_id: 1,
+      evento_id: 1,
       estado: 'Activo',
     },
     {
@@ -30,7 +53,7 @@ export class EventoListar {
       fecha: new Date('2025-12-05T18:00:00'),
       hora_fin: new Date('2025-12-05T20:00:00'),
       aforo: 350,
-      auditorio_id: 2,
+      evento_id: 2,
       estado: 'Pendiente',
     },
   ];
