a823b247f634c04ffbadc9511131d9d3e3d0bf85
Ricomami <don.abraham01@gmail.com>
2025-10-29 07:15:26 -0600

    Integracion de 'entrada/entrada-listar' con coxecion a BDD

--- src/app/entradas/entradas-listar/entradas-listar.ts
+++ src/app/entradas/entradas-listar/entradas-listar.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { Entrada } from '../../interfaces/entrada';
+import { EntradaService } from '../../services/entrada-service';
 
 @Component({
   selector: 'app-entrada-listar',
@@ -10,7 +12,26 @@ import { RouterLink } from '@angular/router';
   styleUrl: './entradas-listar.css'
 })
 export class EntradasListar {
-  entradas = [
+  entradas: Entrada[] = [];
+  
+    constructor(private entradaServicio: EntradaService) { }
+  
+    ngOnInit(): void {
+      this.cargarEntradas();
+    }
+  
+    cargarEntradas() {
+      this.entradaServicio.obtenerEntradas().subscribe({
+        next: (registros: any) => {
+          console.log("Registros devueltos desde API: ", registros);
+          this.entradas = registros.datos;
+        },
+        error: (err) => {
+          console.error("Error al obtener los entradas: ", err);
+        }
+      });
+    }
+  entradass = [
     { id: 1, asiento_id: 40, pago_id:1, funcion_id: 12, cliente_id: 1, precio_final: 250.00, estado: 'Activo' }
   ];
 }
--- src/app/services/entrada-service.ts
+++ src/app/services/entrada-service.ts
@@ -1,8 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Entrada } from '../interfaces/entrada';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EntradaService {
-  
+  private apiURL = "http://localhost:3000/entradas/";
+
+  constructor( private http: HttpClient ) {}
+
+  obtenerEntradas(): Observable<Entrada[]> {
+    return this.http.get<Entrada[]>(this.apiURL);
+  }
+
+  obtenerEntradaPorId(id:number): Observable<Entrada> {
+    return this.http.get<Entrada>(`${this.apiURL}/${id}`);
+  }
+
+  crearEntrada(entrada: Entrada): Observable<Entrada> {
+    return this.http.post<Entrada>(this.apiURL, entrada);
+  }
+
+  actualizarEntrada(id: number, entrada: Entrada): Observable<Entrada> {
+    return this.http.put<Entrada>(`${this.apiURL}/${id}`, entrada);
+  }
+
+  eliminarEntrada(id: number): Observable<any> {
+    return this.http.delete(`${this.apiURL}/${id}`);
+  }
 }
