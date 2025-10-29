ec10a02a1b9b5695893b3988f89ddda25eb6d60b
Ricomami <don.abraham01@gmail.com>
2025-10-29 09:55:54 -0600

    Integracion de 'pago/pago-listar' con conexion a BDD

--- src/app/interfaces/pago.ts
+++ src/app/interfaces/pago.ts
@@ -1,2 +1,10 @@
 export interface Pago {
+    id_pago:  number;
+    cliente_id:  number;
+    metodo_pago:  string;
+    monto:  number;
+    fecha_pago:  string;
+    estado:  string;
+    created_at:  string;
+    updated_at:  string;
 }
--- src/app/pago/pago-listar/pago-listar.ts
+++ src/app/pago/pago-listar/pago-listar.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { Pago } from '../../interfaces/pago';
+import { PagoService } from '../../services/pago-service';
 
 @Component({
   selector: 'app-pago-listar',
@@ -9,7 +11,27 @@ import { RouterLink } from '@angular/router';
   styleUrl: './pago-listar.css'
 })
 export class PagoListar {
-  pagos = [
+  pagos: Pago[] = [];
+  
+    constructor(private pagoServicio: PagoService) { }
+  
+    ngOnInit(): void {
+      this.cargarPagos();
+    }
+  
+    cargarPagos() {
+      this.pagoServicio.obtenerPagos().subscribe({
+        next: (registros: any) => {
+          console.log("Registros devueltos desde API: ", registros);
+          this.pagos = registros.datos;
+        },
+        error: (err) => {
+          console.error("Error al obtener los pagos: ", err);
+        }
+      });
+    }
+
+  pagoss = [
     { id: 1, cliente: "101", metodo: "tarjeta", monto: "1200.00", fecha_pago: "2025-10-15T14:32:11Z", estado: "completado", creado: "2025-10-15T14:32:11Z", actualizado: "2025-10-15T14:32:11Z" },
     { id: 2, cliente: "102", metodo: "efectivo", monto: "850.00", fecha_pago: "2025-10-10T10:25:43Z", estado: "completado", creado: "2025-10-10T10:25:43Z", actualizado: "2025-10-10T10:25:43Z" },
     { id: 3, cliente: "103", metodo: "transferencia", monto: "2000.00", fecha_pago: "2025-10-05T17:41:27Z", estado: "pendiente", creado: "2025-10-05T17:41:27Z", actualizado: "2025-10-07T09:12:03Z" },
