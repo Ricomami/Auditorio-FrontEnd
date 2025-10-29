cdc33dd28c27e180c151935a5b9f607553f73636
Ricomami <don.abraham01@gmail.com>
2025-10-29 10:10:46 -0600

    Integreacion de 'seccion/seccion-listar' con conexion a BDD

--- src/app/interfaces/seccion.ts
+++ src/app/interfaces/seccion.ts
@@ -1,2 +1,10 @@
 export interface Seccion {
+    id_seccion: number;
+    nombre_seccion: string;
+    precio_base: number;
+    auditorio_id: number;
+    imagen_seccion: string;
+    estado: string;
+    created_at: string;
+    updated_at: string;
 }
--- src/app/services/seccion-service.ts
+++ src/app/services/seccion-service.ts
@@ -1,8 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Seccion } from '../interfaces/seccion';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SeccionService {
-  
+  private apiURL = "http://localhost:3000/secciones/";
+
+  constructor( private http: HttpClient ) {}
+
+  obtenerSeccions(): Observable<Seccion[]> {
+    return this.http.get<Seccion[]>(this.apiURL);
+  }
+
+  obtenerSeccionPorId(id:number): Observable<Seccion> {
+    return this.http.get<Seccion>(`${this.apiURL}/${id}`);
+  }
+
+  crearSeccion(seccion: Seccion): Observable<Seccion> {
+    return this.http.post<Seccion>(this.apiURL, seccion);
+  }
+
+  actualizarSeccion(id: number, seccion: Seccion): Observable<Seccion> {
+    return this.http.put<Seccion>(`${this.apiURL}/${id}`, seccion);
+  }
+
+  eliminarSeccion(id: number): Observable<any> {
+    return this.http.delete(`${this.apiURL}/${id}`);
+  }
 }
