5187b9cd1f974683ef25a218d72ba78632b524d8
Ricomami <don.abraham01@gmail.com>
2025-10-31 07:49:21 -0600

    Integracion del modulo 'eventos-artista/eventos-artista-crear' con registro en la BDD

--- src/app/eventos_artista/eventos-artista-crear/eventos-artista-crear.ts
+++ src/app/eventos_artista/eventos-artista-crear/eventos-artista-crear.ts
@@ -1,13 +1,47 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { EventosArtistaService } from '../../services/eventos-artista-service';
+import { EventosArtista } from '../../interfaces/eventos-artista';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-eventos-artista-crear',
-  imports: [RouterLink, CommonModule],
+  imports: [RouterLink, CommonModule, FormsModule], 
   templateUrl: './eventos-artista-crear.html',
   styleUrl: './eventos-artista-crear.css'
 })
 export class EventosArtistaCrear {
+  eventosartista: EventosArtista = {
+    evento_id: 0,
+    artista_id: 0,
+    estado: '',
+    created_at: '',
+    updated_at: ''
+  };
 
+  mensajeExito: string | null = null;
+  mensajeError: string | null = null;
+
+  constructor(private eventosartistaServicio: EventosArtistaService,
+    private route: Router
+  ) { }
+
+  CrearEventosArtista(): void {
+
+    this.mensajeExito = null;
+    this.mensajeError = null;
+
+    this.eventosartistaServicio.crearEventosArtista(this.eventosartista).subscribe({
+      next: (respuesta) => {
+        console.log('Registro de artista en el evento creado correctamente: ', respuesta);
+        alert('Registro de artista en el evento creado exitosamente.');
+        this.route.navigate(['/eventos_artista']); //Redirigimos a la vista de 'listar'
+      },
+      error: (err) => {
+        console.error('Error al crear el eventosartista: ', err);
+        this.mensajeError = 'Ocurrió un error al crear el eventosartista.';
+      }
+    });
+  }
 }
